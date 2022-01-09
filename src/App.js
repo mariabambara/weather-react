8dfd07824c17b5d9985001ddcefb1ee30cf3d3e0
mariabambara <mariafbambara@gmail.com>
2022-01-09 18:07:02 -0500

    Adding app code from CodeSandbox

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <Weather />
-      </header>
+      <h1>Weather App</h1>
+      <Weather />
     </div>
   );
 }
