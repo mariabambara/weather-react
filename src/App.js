bd28ba93b7324cc2662eb7512121299921a2538c
mariabambara <mariafbambara@gmail.com>
2022-01-09 18:12:35 -0500

    Added and styled link to open-sourced code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <p><a href="https://github.com/mariabambara/weather-react" target="_blank" rel="noreferrer">Open-sourced code</a> by <a href="https://mariabambara.ca" target="_blank" rel="noreferrer">Maria Bambara</a>.</p>
     </div>
   );
 }
